1ac3dec8609f4968ad784530c2ec88052d0f89f6
SchneiderMaster <fynn2746@outlook.de>
2023-06-08 18:12:05 +0200

    added the functionality for the "X exp to next level" for the total level and commented most of FirstFragment.java

--- app/src/main/java/com/example/schneider_rpg/FirstFragment.java
+++ app/src/main/java/com/example/schneider_rpg/FirstFragment.java
@@ -41,8 +41,6 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-
-
         return binding.getRoot();
 
     }
@@ -51,25 +49,16 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-//        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                NavHostFragment.findNavController(FirstFragment.this)
-//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-//            }
-//        });
-
+        //set the info for the user at the start
         binding.totalLevelText.setText("LVL. " + sharedPreferences.getInt("level", 0));
         binding.totalLevelBar.setMax(getMaxExperience(sharedPreferences.getInt("level", 0)));
         binding.totalLevelBar.setProgress(sharedPreferences.getInt("experience", 0));
 
+        //update the textView with the exp to the next level
+        binding.totalExpToNextTextView.setText(binding.totalLevelBar.getMax()-binding.totalLevelBar.getProgress() + " exp to next level");
 
-
+        //initiate all the quests and draw them to the screen
         generateQuests();
-
-
-
 
         //initiate all the skills and draw them to the screen
         generateSkills();
@@ -78,11 +67,13 @@ public class FirstFragment extends Fragment {
 
     }
 
-    @SuppressLint("SetTextI18n")
+    //initiate all the skills and draw them to the screen
+    @SuppressLint({"SetTextI18n", "CutPasteId"})
     void generateSkills(){
-        //initiate all the skills and draw them to the screen
+
         binding.skills.removeAllViews();
 
+        //create the titleTextView and give it the necessary data
         TextView textView = new TextView(getContext());
         textView.setId(R.id.skillsTitle);
         textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -90,39 +81,38 @@ public class FirstFragment extends Fragment {
         textView.setTextAppearance(R.style.Base_Theme_SchneiderRPG_Title);
         textView.setGravity(Gravity.START);
 
-// If you are using a ConstraintLayout, set the constraints programmatically
+        //set the constraints of the titleTextView
         ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
         layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
         layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
         textView.setLayoutParams(layoutParams);
 
-// Add the TextView to the parent view
+        //add the titleTextView to the parent view
         binding.skills.addView(textView);
 
 
-
+        //iterate through all skills in the database and draw them individually to the screen by accessing a layout and putting all of the information into it
         List<Skill> skills = skillDao.getAll();
         for(int i = 0; i < skillDao.count(); i++) {
             ConstraintLayout skillLayout = binding.skills;
             @SuppressLint("InflateParams") View includeView = getLayoutInflater().inflate(R.layout.skill_layout, null);
 
 
-
-            TextView title = (TextView) includeView.findViewById(R.id.skill_title);
+            //create the textView for the name of the skill and format it correctly
+            TextView title = includeView.findViewById(R.id.skill_title);
             title.setText(skills.get(i).name + " LVL. " + skills.get(i).level);
             title.setTextColor(skills.get(i).color);
 
 
-
-            ProgressBar bar = (ProgressBar) includeView.findViewById(R.id.skillProgress);
+            //create the progressBar for the skill progress
+            ProgressBar bar = includeView.findViewById(R.id.skillProgress);
             bar.setProgressTintList(ColorStateList.valueOf(skills.get(i).color));
             bar.setMax(getMaxExperience(skills.get(i).level));
             bar.setProgress((skills.get(i).experience));
 
 
-
-            Button button = (Button) includeView.findViewById(R.id.addHourButton);
-
+            //create the button that allows the user to add progress to the quest and add the logic
+            Button button = includeView.findViewById(R.id.addHourButton);
             int finalI = i;
             while(skills.get(finalI).experience >= getMaxExperience(skills.get(finalI).level)){
 
@@ -133,10 +123,10 @@ public class FirstFragment extends Fragment {
 
                 skills = skillDao.getAll();
 
-                TextView title1 = (TextView) includeView.findViewById(R.id.skill_title);
+                TextView title1 = includeView.findViewById(R.id.skill_title);
                 title1.setText(skills.get(finalI).name + " LVL. " + skills.get(finalI).level);
 
-                ProgressBar bar3 = (ProgressBar) includeView.findViewById(R.id.skillProgress);
+                ProgressBar bar3 = includeView.findViewById(R.id.skillProgress);
                 bar3.setMax(getMaxExperience(skills.get(finalI).level));
                 bar3.setProgress((skills.get(finalI).experience));
 
@@ -147,8 +137,6 @@ public class FirstFragment extends Fragment {
 
                 updateTotalLevel(10);
 
-                Log.d("aöslkdjf", "level: " + sharedPreferences.getInt("level", 0) + "; progress: " + sharedPreferences.getInt("experience", 0));
-
                 List<Skill> skills1 = skillDao.getAll();
                 skillDao.addHour(skills1.get(finalI).id);
 
@@ -157,8 +145,6 @@ public class FirstFragment extends Fragment {
                 bar1.setProgress((skills1.get(finalI).experience));
 
                 skills1 = skillDao.getAll();
-
-                Log.e("test", "id: " + skills1.get(finalI).id + "; xp: " + skills1.get(finalI).experience + "; maxNumber: " + getMaxExperience(skills1.get(finalI).level));
 
                 while(skills1.get(finalI).experience >= getMaxExperience(skills1.get(finalI).level)){
 
@@ -201,11 +187,12 @@ public class FirstFragment extends Fragment {
         }
     }
 
+    //initiate all the quests and draw them to the screen
     @SuppressLint("SetTextI18n")
     void generateQuests(){
-        //initiate all the skills and draw them to the screen
         binding.quests.removeAllViews();
 
+        //create the titleTextView and give it the necessary data
         TextView textView = new TextView(getContext());
         textView.setId(R.id.questTitle);
         textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -213,25 +200,25 @@ public class FirstFragment extends Fragment {
         textView.setTextAppearance(R.style.Base_Theme_SchneiderRPG_Title);
         textView.setGravity(Gravity.START);
 
-// If you are using a ConstraintLayout, set the constraints programmatically
+        //set the constraints of the titleTextView
         ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
         layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
         layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
         textView.setLayoutParams(layoutParams);
 
-// Add the TextView to the parent view
+        //add the titleTextView to the parent view
         binding.quests.addView(textView);
 
 
 
-        //initiate all the quests and draw them to the screen
+        //iterate through all quests in the database and draw them individually to the screen by accessing a layout and putting all of the information into it
         List<Quest> quests = questDao.getAll();
         for(int i = 0; i < questDao.count(); i++){
             ConstraintLayout questLayout = binding.quests;
             @SuppressLint("InflateParams") View includeView = getLayoutInflater().inflate(R.layout.quest_layout, null);
 
 
-
+            //create the textView for the name of the quest and format it correctly
             TextView questName = includeView.findViewById(R.id.questName);
             String fullText = quests.get(i).name + " (" + quests.get(i).reward + " " + skillDao.getNameFromId(quests.get(i).skillId) + "-Exp, " + quests.get(i).progress + "/" + quests.get(i).max + ")";
             Log.e("test ", "fullText: " + fullText);
@@ -243,13 +230,13 @@ public class FirstFragment extends Fragment {
             questName.setText(spannableStringBuilder);
 
 
-
+            //create the progressBar for the quest progress
             ProgressBar bar = (ProgressBar) includeView.findViewById(R.id.questProgress);
             bar.setProgressTintList(ColorStateList.valueOf(skillDao.getColorFromId(quests.get(i).skillId)));
             bar.setMax(quests.get(i).max);
             bar.setProgress(quests.get(i).progress);
 
-
+            //create the button that allows the user to add progress to the quest and add the logic
             Button button = (Button) includeView.findViewById(R.id.addProgressButton);
             int finalI = i;
             button.setOnClickListener(v -> {
@@ -267,24 +254,24 @@ public class FirstFragment extends Fragment {
                 questName.setText(spannableStringBuilder1);
                 bar.setProgress(quests1.get(finalI).progress);
 
+                // if the level is completed, increase the level and update the exp of both the total score and the skill
                 if(quests1.get(finalI).progress >= quests1.get(finalI).max){
+                    //update total
                     updateTotalLevel(quests1.get(finalI).reward);
-
+                    //user message
                     Toast.makeText(getContext(), "You completed a quest and gained " + quests1.get(finalI).reward + " " + skillDao.getNameFromId(quests1.get(finalI).skillId) + "-experience!", Toast.LENGTH_LONG).show();
-
+                    //update the skill and quest database
                     skillDao.addXP(quests1.get(finalI).reward, quests1.get(finalI).skillId);
-
-
-
-
                     questDao.deleteQuest(quests1.get(finalI).id);
 
+                    //update GUI
                     generateQuests();
                     generateSkills();
 
                 }
             });
 
+            //set the icon of the quest button to a checkmark if the quest only needs one repetition to be completed
             if (quests.get(i).max == 1){
                 button.setText("✓");
                 button.setTextColor(Color.GREEN);
@@ -293,7 +280,7 @@ public class FirstFragment extends Fragment {
 
 
 
-
+            //set the correct constraints for the ConstraintLayout and other visual minorities
             ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
             if(i == 0) {
                 params.topToBottom = R.id.questTitle;
@@ -304,7 +291,11 @@ public class FirstFragment extends Fragment {
             params.topMargin = 16;
             params.bottomMargin = 16;
             includeView.setLayoutParams(params);
+
+            //update the id of the added layout
             includeView.setId((R.layout.quest_layout) + quests.get(i).id);
+
+            //finally add the layout to the Quest container
             questLayout.addView(includeView);
         }
 
@@ -313,32 +304,33 @@ public class FirstFragment extends Fragment {
 
     @SuppressLint("SetTextI18n")
     void updateTotalLevel(int gainedExp){
+
         int progress = binding.totalLevelBar.getProgress() + gainedExp;
-
         binding.totalLevelBar.setProgress(progress);
-        Log.d("progress", "progress: " + binding.totalLevelBar.getProgress() + "; maxNumber: " + binding.totalLevelBar.getMax());
 
-
+        //increase the level and update everything if the level is completed
         while(progress >= binding.totalLevelBar.getMax()) {
-            Log.e("test", "increased a level!!!!!!!");
+            binding.totalLevelBar.setProgress(progress - getMaxExperience(sharedPreferences.getInt("level", 0)));
+            progress = binding.totalLevelBar.getProgress();
             editor.putInt("level", sharedPreferences.getInt("level", 0) + 1);
             editor.apply();
             binding.totalLevelText.setText("LVL. " + sharedPreferences.getInt("level", 0));
-            binding.totalLevelBar.setProgress(progress - getMaxExperience(sharedPreferences.getInt("level", 0)));
             binding.totalLevelBar.setMax(getMaxExperience(sharedPreferences.getInt("level", 0)));
+            editor.putInt("experience", binding.totalLevelBar.getProgress());
+            editor.apply();
         }
 
-
+        //update the textView with the exp to the next level
+        binding.totalExpToNextTextView.setText(binding.totalLevelBar.getMax()-binding.totalLevelBar.getProgress() + " exp to next level");
 
         editor.putInt("experience", binding.totalLevelBar.getProgress());
         editor.apply();
 
     }
 
+    //method to calculate the maximum amount of exp for a set level
     Integer getMaxExperience(int currentLevel){
-
         return currentLevel*currentLevel/4 +10;
-
     }
 
     @Override
